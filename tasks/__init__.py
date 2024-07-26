1142f99ae368c0cb680c86a964552132352b2745
Abhrant <abhiranta@nyunai.com>
2024-07-26 16:13:17 +0000

    fix: distributed training SSF bugs

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,9 +1,34 @@
-from .text_generation import CausalLLM
-from .image_classification import ImageClassification
-from .timm_image_classification import TimmforImageClassification
-from .text_classification import SequenceClassification
-from .summarization import Seq2Seq
-from .translation import Translation
-from .question_answering import QuestionAnswering
 from .params import create_instance, AdaptParams
 
+
+def initialize_initialization(task, subtask):
+    if task == "text_generation":
+        from nyuntam_adapt.tasks.text_generation import CausalLLM
+
+        return CausalLLM
+
+    elif task == "text_classification":
+        from nyuntam_adapt.tasks.text_classification import SequenceClassification
+
+        return SequenceClassification
+
+    elif task == "Seq2Seq_tasks":
+        if subtask == "translation":
+            from nyuntam_adapt.tasks.translation import Translation
+
+            return Translation
+
+        elif subtask == "summarization":
+            from nyuntam_adapt.tasks.summarization import Seq2Seq
+
+            return Seq2Seq
+
+    elif task in ["question_answering", "Question Answering"]:
+        from nyuntam_adapt.tasks.question_answering import QuestionAnswering
+
+        return QuestionAnswering
+
+    elif task == "image_classification":
+        from nyuntam_adapt.tasks.image_classification import ImageClassification
+
+        return ImageClassification
