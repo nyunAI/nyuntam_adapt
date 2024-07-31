d6a45648544d22359bcb8a8abb851af34cc6020b
Abhrant <abhiranta@nyunai.com>
2024-07-31 16:59:13 +0000

    ✨feat: update mmseg without submodule

--- tasks/image_segmentation_mmseg/custom_dataset.py
+++ tasks/image_segmentation_mmseg/custom_dataset.py
@@ -4,19 +4,7 @@ from mmseg.datasets import BaseSegDataset
 
 @DATASETS.register_module()
 class CustomDataset(BaseSegDataset):
-    METAINFO = dict(
-        classes=("sky", "tree", "road", "grass", "water", "bldg", "mntn", "fg obj"),
-        palette=[
-            [128, 128, 128],
-            [129, 127, 38],
-            [120, 69, 125],
-            [53, 125, 34],
-            [0, 11, 123],
-            [118, 20, 12],
-            [122, 81, 25],
-            [241, 134, 51],
-        ],
-    )
+    METAINFO = dict(classes=(), palette=[])
 
     def _init_(self, **kwargs):
         super()._init_(img_suffix=".jpg", seg_map_suffix=".png", **kwargs)
